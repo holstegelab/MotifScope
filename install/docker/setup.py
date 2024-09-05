d15407605fb12fd9cf6347eab79f97350449fc68
Yaran Zhang <holstegelab-yzhang@ui-02.spider.surfsara.nl>
2024-09-05 14:25:18 +0200

    fixed bug

---
+++ install/docker/setup.py
@@ -0,0 +1,20 @@
+import os
+from setuptools import setup, find_packages
+
+os.chdir(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+here = os.getcwd()
+main_script = os.path.join(here, 'motifscope')
+
+packages = find_packages(where=here, include=['mscope', 'mscope.*'])
+
+setup(
+    name="MotifScope",
+    version="0.2.0",
+    packages=packages,
+    scripts=[main_script],
+    install_requires=[
+        'numpy','pandas','matplotlib','biopython','multiprocess','umap-learn','scikit-learn','levenshtein','pyabpoa'],
+    author="Y.Zhang",
+    author_email="y.zhang1@amsterdamumc.nl",
+    description="MotifScope is a tool to characterize and visualize motif composition of tandem repeats"
+)
